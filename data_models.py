c54925c41f3097198554a9cc411e3a0982aa88fd
Thorsten Kelterborn <tk@friendsandeye.de>
2026-09-01 13:06:15 +0200

    Add data_models.py with classes Author and Book, create db

---
+++ data_models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    __tablename__ = "authors"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
+    date_of_death = db.Column(db.Date, nullable=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Book(db.Model):
+    __tablename__ = "books"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(13), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+
+    def __str__(self):
+        return self.title
